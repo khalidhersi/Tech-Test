8e42eccbe2ce2d0a644b7b283ca2c666f6b5484e
khalidhersi <cipherpolfoundation@gmail.com>
2021-11-24 16:54:07 +0000

    js DOM elements declared

---
+++ main.js
@@ -0,0 +1,5 @@
+const logo = document.querySelector(".nav__logo");
+const reset = document.querySelector(".nav__btn");
+const submitBtn = document.querySelector(".section__submit");
+const taskInput = document.querySelector(".main__input");
+const tast = document.querySelector(".section__p");
