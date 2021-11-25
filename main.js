4e3c60db8f881163d422fb5b8005dd2cf454a4c1
khalidhersi <cipherpolfoundation@gmail.com>
2021-11-25 10:07:33 +0000

    Toto list works

--- main.js
+++ main.js
@@ -1,5 +1,18 @@
 const logo = document.querySelector(".nav__logo");
 const reset = document.querySelector(".nav__btn");
-const submitBtn = document.querySelector(".section__submit");
+const submitBtn = document.querySelector(".main__submit");
 const taskInput = document.querySelector(".main__input");
-const tast = document.querySelector(".section__p");
+const taskOutput = document.querySelector(".section__p");
+const taskSection = document.querySelector(".section-task");
+const deleteBtn = document.querySelector(".delete__btn");
+
+
+let listArr = []
+
+submitBtn.addEventListener("click", () => {
+    listArr.push(taskInput)
+    const newTask = document.createElement("ul");
+    newTask.setAttribute("class", "new__task")
+    taskSection.appendChild(newTask)
+    newTask.innerHTML = `<li> <input type="checkbox"> ${taskInput.value} </li>`
+})
